d8d3bf4df8d1fea454e24c5d146260f9219db235
LongVu80 <thienlong14th@gmail.com>
2021-09-24 22:52:02 -0500

    fixed the javascript for better function. still need more work to do

--- js/script.js
+++ js/script.js
@@ -25,131 +25,226 @@ function logIn(){
       }
 
       $(".women").click(function(){
-        $("#women").fadeToggle();
+        $("#woman").show();
+        $("#man").hide();
+        $("#bling").hide();
+        $("#electronics").hide();
     });
 
     $(".men").click(function(){
-      $("#men").fadeToggle();
+      $("#woman").hide();
+        $("#man").show();
+        $("#bling").hide();
+        $("#electronics").hide();
   });
 
-  $(".jewelry").click(function(){
-    $("#jewelry").fadeToggle();
+  $(".jewelery").click(function(){
+    $("#woman").hide();
+    $("#man").hide();
+    $("#bling").show();
+    $("#electronics").hide();
 });
 
 $(".electronic").click(function(){
-  $("#electronic").fadeToggle();
+  $("#woman").hide();
+  $("#man").hide();
+  $("#bling").hide();
+  $("#electronics").show();
 });
-    $(document).ready(function() {
+$(document).ready(function() {
+	$.get('https://fakestoreapi.com/products', function (data) {
+		console.log(data[0].title);
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].category === "men's clothing") {
+				console.log('hello');
+				$('#men').append(`
+        <div class='card m-2 border-2' style='width:200px;'>
+        <img src='${data[i].image}' class='fake'>
+        <div class='card-body'>
+        <p class ='card-title' id='productTitle'> ${data[i].title} </p>
+        <p class='card-text' id='productPrice'> $${data[i].price} </p>
+        
+        <div>
+            <form class='form'>
+                <input type="number" id='input${data[i].id}' class="itemQuantity" name="tentacles" min="0" max="100" placeholder='0' class='price'>
+                <button id='${data[i].id}' type='submit' class='add'>Add To Cart</button>
+            </form>
+            
+        </div>
+                `);
+			} else if (data[i].category === "women's clothing") {
+				console.log('hello');
+				$('#women').append(`
+                    <div class='card m-2 border-2' style='width:200px;'>
+                    <img src='${data[i].image}' class='fake'>
+                    <div class='card-body'>
+                    <p class ='card-title' id='productTitle'> ${data[i].title} </p>
+                    <p class='card-text' id='productPrice'> $${data[i].price} </p>
+                    
+                    <div>
+                        <form class='form'>
+                            <input type="number" id='input${data[i].id}' class="itemQuantity" name="tentacles" min="0" max="100" placeholder='0' class='price'>
+                            <button id='${data[i].id}' type='submit' class='add'>Add To Cart</button>
+                        </form>
+                        
+                    </div>
+                `);
+			} else if (data[i].category === 'jewelery') {
+				console.log('hello');
+				$('#jewelery').append(`
+        <div class='card m-2 border-2' style='width:200px;'>
+        <img src='${data[i].image}' class='fake'>
+        <div class='card-body'>
+        <p class ='card-title' id='productTitle'> ${data[i].title} </p>
+        <p class='card-text' id='productPrice'> $${data[i].price} </p>
+        
+        <div>
+            <form class='form'>
+                <input type="number" id='input${data[i].id}' class="itemQuantity" name="tentacles" min="0" max="100" placeholder='0' class='price'>
+                <button id='${data[i].id}' type='submit' class='add'>Add To Cart</button>
+            </form>
+            
+        </div>
+                `);
+			} else if (data[i].category === 'electronics') {
+				console.log('hello');
+				$('#electronic').append(`
+        <div class='card m-2 border-2' style='width:200px;'>
+        <img src='${data[i].image}' class='fake'>
+        <div class='card-body'>
+        <p class ='card-title' id='productTitle'> ${data[i].title} </p>
+        <p class='card-text' id='productPrice'> $${data[i].price} </p>
+        
+        <div>
+            <form class='form'>
+                <input type="number" id='input${data[i].id}' class="itemQuantity" name="tentacles" min="0" max="100" placeholder='0' class='price'>
+                <button id='${data[i].id}' type='submit' class='add'>Add To Cart</button>
+            </form>
+            
+        </div>
+                `);
+			};
+    };
+  });
+		
       // (Suppose to be this link for category women clothing)
       // $.get("https://storetester.herokuapp.com/api/products/category/women_clothing", function(people) {   
-        $.get("https://storetester.herokuapp.com/api/products/", function(people) {
-          var data = people.data;
-          var htmlStr = "";
-          for(var i = 11; i <=16; i++) {
-            var name = data[i].title;
-            var price = data[i].price
+      //   $.get("https://fakestoreapi.com/products", function(data) {
+      //     // var data = people.data;
+      //     var htmlStr = "";
+      //     for(var i = 0; i <20; i++) {
+      //       var name = data[i].title;
+      //       var price = data[i].price
+      //       // var description = data[i].description
 
-            htmlStr += "<div class='female m-2 border-0' style='width:200px;'>";
-            htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake'>";
-            htmlStr += "<div class='card-body'>";
-            htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
-            htmlStr += "<p class='card-text'>"+ "$" + price + "</p>"; 
-            htmlStr += "</div>";
-            htmlStr += "</div>";
-        };
-        $("#women").append(htmlStr);
-      });
+      //       htmlStr += "<div class='female m-2 border-2' style='width:200px;'>";
+      //       htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake' style='padding:5px'>";
+      //       htmlStr += "<div class='card-body'>";
+      //       htmlStr += "<h5 class='card-title'>" + name + "</h5>";
+      //       htmlStr += "<p class='card-text'>"+ "$" + price + "</p>";
+      //       // htmlStr += "<p class='card-text'>" + description + "</>" 
+      //       htmlStr += "</div>";
+      //       htmlStr += "</div>";
+      //   };
+      //   $("#women").append(htmlStr);
+      // });
 
       // (Suppose to be this link for category men_clothing)
       // $.get("https://storetester.herokuapp.com/api/products/category/men_clothing", function(people) {   
-        $.get("https://storetester.herokuapp.com/api/products", function(people) {
-          var data = people.data;
-          var htmlStr = "";
+      //   $.get("https://fakestoreapi.com/products", function(data) {
+      //     //var data = people.data;
+      //     // var htmlStr = "";
 
-          for(var i = 0; i <=0; i++) {
-            var name = data[0].title;
-            var price = data[0].price
+      //   //   for(var i = 0; i <=0; i++) {
+      //   //     var name = data[0].title;
+      //   //     var price = data[0].price
 
-            htmlStr += "<div class='male m-2 border-0' style='width:200px;'>";
-            htmlStr += "<img src=" + data[0].image +" alt='product image' class='fake'>";
-            htmlStr += "<div class='card-body'>";
-            htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
-            htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
-            htmlStr += "</div>";
-            htmlStr += "</div>";
-        };
-        $("#men").attr(htmlStr);
+      //   //     htmlStr += "<div class='male m-2 border-0' style='width:200px;'>";
+      //   //     htmlStr += "<img src=" + data[0].image +" alt='product image' class='fake'>";
+      //   //     htmlStr += "<div class='card-body margin:10px'>";
+      //   //     htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
+      //   //     htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
+      //   //     htmlStr += "</div>";
+      //   //     htmlStr += "</div>";
+      //   // };
+      //   // $("#men").attr(htmlStr);
         
-          for(var i = 18; i < 20; i++) {
-              var name = data[i].title;
-              var price = data[i].price
+      //     for(var i = 0; i < 20; i++) {
 
-              htmlStr += "<div class='male m-2 border-0' style='width:200px;'>";
-              htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake'>";
-              htmlStr += "<div class='card-body'>";
-              htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
-              htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
-              htmlStr += "</div>";
-              htmlStr += "</div>";
-          };
-          $("#men").append(htmlStr);
+      //         // var name = data[i].title;
+      //         // var price = data[i].price
+      //         if (data[i].category === "men's clothing") {
+      //           $('#men').append(`
+      //         <p id='productTitle'> ${data[i].title} </p>
+      //         <p id='productDescription'> ${data[i].description} </p>
+      //         <img src='${data[i].image}'>
+      //         <div>
+      //             <form class='form'>
+      //                 <input type="number" id='input${data[i].id}' class="itemQuantity" name="tentacles" min="0" max="100" placeholder='0'>
+      //                 <button id='${data[i].id}' type='submit'>Add To Cart</button>
+      //             </form>
+      //             <p id='productPrice'> $${data[i].price} </p>
+      //         </div>
+      //         <hr />
+      //     `);
+      //     };
+          
 
-      });
+      // });
 
       // (Suppose to be this link for category jewelry)
       // $.get("https://storetester.herokuapp.com/api/products/category/jewelry", function(people) {   
-        $.get("https://storetester.herokuapp.com/api/products", function(people) {
-          var data = people.data;
-          var htmlStr = "";
+      //   $.get("https://fakestoreapi.com/products", function(data) {
+      //     var htmlStr = "";
         
-          for(var i = 1; i < 5; i++) {
-              var name = data[i].title;
-              var price = data[i].price
+      //     for(var i = 1; i < 5; i++) {
+      //         var name = data[i].title;
+      //         var price = data[i].price
+              
+      //         htmlStr += "<div class='bling m-2 border-0' style='width:200px;'>";
+      //         htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake'>";
+      //         htmlStr += "<div class='card-body'>";
+      //         htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
+      //         htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
+      //         htmlStr += "</div>";
+      //         htmlStr += "</div>";
+      //     };
+      //     $("#jewelry").append(htmlStr);
+      // });
 
-              htmlStr += "<div class='bling m-2 border-0' style='width:200px;'>";
-              htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake'>";
-              htmlStr += "<div class='card-body'>";
-              htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
-              htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
-              htmlStr += "</div>";
-              htmlStr += "</div>";
-          };
-          $("#jewelry").append(htmlStr);
-      });
+      // // (Suppose to be this link for category electronic)
+      // // $.get("https://storetester.herokuapp.com/api/products/category/electronic", function(people) {   
+      // $.get("https://fakestoreapi.com/products", function(data) {
+          
+      //     var htmlStr = "";
+      //     for(var i = 0; i <=0; i++) {
+      //       var name = data[17].title;
+      //       var price = data[17].price
 
-      // (Suppose to be this link for category electronic)
-      // $.get("https://storetester.herokuapp.com/api/products/category/electronic", function(people) {   
-      $.get("https://storetester.herokuapp.com/api/products", function(people) {
-          var data = people.data;
-          var htmlStr = "";
-          for(var i = 0; i <=0; i++) {
-            var name = data[17].title;
-            var price = data[17].price
+      //       htmlStr += "<div class='male m-2 border-0' style='width:200px;'>";
+      //       htmlStr += "<img src=" + data[17].image +" alt='product image' class='fake'>";
+      //       htmlStr += "<div class='card-body'>";
+      //       htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
+      //       htmlStr += "<p class='card-text'>" +"$" + price + "</p>"; 
+      //       htmlStr += "</div>";
+      //       htmlStr += "</div>";
+      //     };
+      //     $("#electronic").attr(htmlStr);
 
-            htmlStr += "<div class='male m-2 border-0' style='width:200px;'>";
-            htmlStr += "<img src=" + data[17].image +" alt='product image' class='fake'>";
-            htmlStr += "<div class='card-body'>";
-            htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
-            htmlStr += "<p class='card-text'>" +"$" + price + "</p>"; 
-            htmlStr += "</div>";
-            htmlStr += "</div>";
-          };
-          $("#electronic").attr(htmlStr);
+      //     for(var i = 5; i < 11; i++) {
+      //         var name = data[i].title;
+      //         var price = data[i].price
 
-          for(var i = 5; i < 11; i++) {
-              var name = data[i].title;
-              var price = data[i].price
-
-              htmlStr += "<div class='electric m-2 border-0' style='width:200px;'>";
-              htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake'>";
-              htmlStr += "<div class='card-body'>";
-              htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
-              htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
-              htmlStr += "</div>";
-              htmlStr += "</div>";
-          };
-          $("#electronic").append(htmlStr);
-      }, "json");
+      //         htmlStr += "<div class='electric m-2 border-0' style='width:200px;'>";
+      //         htmlStr += "<img src=" + data[i].image +" alt='product image' class='fake'>";
+      //         htmlStr += "<div class='card-body'>";
+      //         htmlStr += "<h5 class='card-title margin:10px'>" + name + "</h5>";
+      //         htmlStr += "<p class='card-text'>" + "$" + price + "</p>"; 
+      //         htmlStr += "</div>";
+      //         htmlStr += "</div>";
+      //     };
+      //     $("#electronic").append(htmlStr);
+      // }, "json");
 
       var apiKey = '6b71e45e81339efb68c7b8d995519887'
 
@@ -169,5 +264,5 @@ $(".electronic").click(function(){
         }, 'json');
         return false;
     });
-  });
   
+});
